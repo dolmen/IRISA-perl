e54296959319500d06a6432ae367053e7e1ba01a
Olivier Mengué <dolmen@cpan.org>
2010-09-30 00:13:28 +0200

    Added copyright and license.

--- lib/IRISA.pm
+++ lib/IRISA.pm
@@ -10,4 +10,22 @@ __END__
 
 IRISA - Pure Perl interface to communicate with IRISA servers (DRI)
 
+=head1 SEE ALSO
+
+L<IRISA::Interface::DSL>
+
+L<IRISA::Interface::Registry>
+
+=head1 AUTHOR
+
+Olivier MenguE<eacute> E<lt>dolmen@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+Copyright E<copy> 2010 Olivier MenguE<eacute>.
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself, either Perl version 5.12.0 or, at your option,
+any later version of Perl 5 you may have available.
+
 =cut
