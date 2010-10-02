6cdc211aae1d32a7ecc6cccc66e0d605c3fc4e12
Olivier Mengué <dolmen@cpan.org>
2010-10-02 14:04:57 +0200

    IRISA::Arg is now obsolete.

--- IRISA-Interface/lib/IRISA/Arg.pm
+++ IRISA-Interface/lib/IRISA/Arg.pm
@@ -2,42 +2,4 @@ use strict;
 
 package IRISA::Arg;
 
-
-
-
-sub encode
-{
-    my ($id, $type, @value) = (@_);
-    eval "require $type";
-    my ($prefix, $data) = do {
-        no strict 'refs';
-        &{$type.'::encode'}(@value)
-    };
-    pack('Cn', $prefix, $id).$data;
-}
-
-# Params: ($raw_data)
-# Returns: ($id, $length, $value)
-sub decode
-{
-    my ($d, $type) = (@_); # TODO extract the type from the Arg registry
-    my ($prefix, $id, $data) = unpack('Cna*', $d);
-    my $map = do {
-        no strict 'refs';
-        &{$type.'::decode_map'}()
-    };
-    if (! exists $map->{$prefix}) {
-        die "Invalid data: prefix does not match expected type";
-    }
-    my $length;
-    my $dec = $map->{$prefix};
-    if (ref($dec) eq '') {
-        return ($id, 0, $dec);
-    } elsif (ref($dec) eq 'CODE') {
-        return ($id, $dec->($data));
-    } else {
-        die "$type: Unexpected value in decode_map for prefix $prefix";
-    }
-}
-
 1;  # vim: set et ts=4 sw=4 sts=4 :
