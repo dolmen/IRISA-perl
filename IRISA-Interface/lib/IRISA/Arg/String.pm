bfb3ea0fac5bfdfcbe1e8e960f2135b312455d97
Olivier Mengué <dolmen@cpan.org>
2010-10-02 10:44:13 +0200

    Fix String arg decoding.

--- IRISA-Interface/lib/IRISA/Arg/String.pm
+++ IRISA-Interface/lib/IRISA/Arg/String.pm
@@ -15,15 +15,17 @@ sub encode
 {
     my $decode_map = {
         6  => sub($) {
-            #my $l = ord($_[0]);
-            #return (1+$l, substr($_[0], 1, $l));
-            unpack('C/a*', $_[0]);
+            my $l = ord($_[0]);
+            die "Missing data in String arg\n" if length($_[0]) < 1+$l;
+            (1+$l, substr($_[0], 1, $l))
+            #(1+$l, unpack('C/a*', $_[0]));
         },
         10 => '',
         12 => sub($) {
-            #my $l = unpack('n', $_[0]);
-            #return (2+$l, substr($_[0], 2, $l));
-            unpack('n/a*', $_[0]);
+            my $l = unpack('n', $_[0]);
+            die "Missing data in String arg\n" if length($_[0]) < 1+$l;
+            (2+$l, substr($_[0], 2, $l))
+            #(1+$l, unpack('n/a*', $_[0]))
         }
     };
 
