15963d9d3f13d948df5acd3913a3af581aa1e25e
Olivier Mengué <dolmen@cpan.org>
2010-10-02 14:26:47 +0200

    IRISA::Interface: delete obsolete code

--- IRISA-Interface/lib/IRISA/Interface.pm
+++ IRISA-Interface/lib/IRISA/Interface.pm
@@ -9,9 +9,6 @@ use Scalar::Util;
 
 
 
-my %arg_registry = ('' => {});
-my %msg_registry;
-
 my @types = qw/Int Bool Date String Buffer Real Char IntTable BufferTable ArgTable/;
 my %types;
 {
@@ -26,74 +23,10 @@ my %types;
 
 sub name
 {
-    my ($pkg, $file, $line) = caller;
-    ${$pkg.'::name'} = $_[0];
+    my ($class) = shift;
+    no strict 'refs';
+    ${$class.'::name'}
 }
 
-
-sub arg
-{
-    my ($class, $intf, $name, $type, $id) = @_;
-    $type = $types{$type};
-    my $info = [ $name, $id, $type ];
-    $arg_registry{$intf} = { } unless exists $arg_registry{$intf};
-    $arg_registry{$intf}{$name} = $info;
-    $arg_registry{$id} = $info;  # TODO handle conflicts
-    return;
-}
-
-
-sub arg_id
-{
-    my $id = shift;
-    if (looks_like_number($id)) {
-		return $id;
-	}
-    if (exists $arg_registry{''}{$id}) {
-		return $arg_registry{''}{$id}->[1];
-	}
-    #if (defined $interface && exists $arg_registry{$interface}{$id}) {
-    #   return $arg_registry{$interface}{$id}->[1];
-	#}
-    croak "unknown ";
-}
-
-sub arg_type
-{
-    my $id = shift;
-    $arg_registry{$id}->[1];
-}
-
-sub arg_info
-{
-    my ($id, $value) = @_;
-    $id = arg_id($id);
-    croak "unknown arg '$id'!" unless exists $arg_registry{$id};
-    tie my $data, $arg_registry{id}->[1], $value;
-    $id = $arg_registry{$id}->[0];
-    return wantarray ? ($id, $data) : [ $id, $data ];
-}
-
-sub message
-{
-    
-}
-
-sub args_encode
-{
-    my $class = shift;
-    my %args = @_;
-}
-
-sub message_encode
-{
-    my $class = shift;
-    my $message = shift;
-    my %args = @_;
-}
-
-sub message_decode
-{
-}
 
 1;
